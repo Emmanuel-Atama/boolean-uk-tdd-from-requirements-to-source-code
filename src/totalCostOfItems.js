9ac893f994e955e193ae713a41700d27f7d2dbe7
Emmanuel Atama <emmanuelatama@outlook.com>
2021-11-01 17:10:49 +0000

    Recent Updates to totalCostOfItems.js

--- src/totalCostOfItems.js
+++ src/totalCostOfItems.js
@@ -1,6 +1,6 @@
 function totalCostOfItems(basket) {
     let total = 0
-    basket.forEach((item) => (total += item.price * item.quantity))
+    basket.forEach((item) => (total = total + item.price * item.quantity))
 
     return total
 }
